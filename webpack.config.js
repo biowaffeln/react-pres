023d093c2188d15ccb28fe3ef3673e547b5b7611
Mark Kvetny <m.kvetny@outlook.com>
2020-03-06 23:54:31 +0000

    rename package and remove style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,6 @@ module.exports = {
         test: /\.tsx$/,
         use: "babel-loader",
         exclude: /node_modules/
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
       }
     ]
   },
